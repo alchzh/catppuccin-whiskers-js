69a95255ab27fc28b22fa373e754e231a41d3f4f
Albert Zhang <alchzh@gmail.com>
2023-11-27 06:01:14 -0500

    Rename to catppuccin-whiskers-js

--- publish-scoped.sh
+++ publish-scoped.sh
@@ -2,8 +2,9 @@
 
 npm run prepare
 mv package.json package.json.tmp
-jq '.name = "@alchzh/"+.name' package.json.tmp > package.json
-npm publish --ignore-scripts
+jq '.name = "@alchzh/catppuccin-whiskers-js" | del(.scripts.prepare)' package.json.tmp > package.json
+cp package.json dist/package.json
+pnpm publish --ignore-scripts --no-git-checks
 cp package.json.tmp package.json
 cp package.json dist/package.json
 rm package.json.tmp
